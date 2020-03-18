1f0d1b0c76f885acb0501626fc062ca2fe51710d
Yu-Wen Pu <ywpu@cs.nctu.edu.tw>
2020-03-19 04:40:11 +0800

    按 Tab 鍵暫停／繼續遊戲

---
+++ Assets/PauseGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour {
+
+    private bool _isPaused;
+
+    private void Start() {
+        _isPaused = false;
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Tab)) {
+            Time.timeScale = _isPaused ? 1 : 0;
+            _isPaused = !_isPaused;
+        }
+    }
+
+}
