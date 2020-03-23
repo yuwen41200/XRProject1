e5eb9e93e6f30a8b98df77acebc4a3e5cdc3f797
Yu-Wen Pu <ywpu@cs.nctu.edu.tw>
2020-03-24 00:36:36 +0800

    HTC VIVE VR TEST

---
+++ Assets/ViveController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class ViveController : MonoBehaviour {
+
+    public SteamVR_Action_Boolean playerAction;
+    public Hand hand;
+    public GameObject obj;
+
+    private void OnEnable() {
+        if (hand == null)
+            hand = GetComponent<Hand>();
+        if (playerAction == null)
+            return;
+        playerAction.AddOnChangeListener(OnPlayerActionChange, hand.handType);
+    }
+
+    private void OnDisable() {
+        if (playerAction != null)
+            playerAction.RemoveOnChangeListener(OnPlayerActionChange, hand.handType);
+    }
+
+    private void OnPlayerActionChange(SteamVR_Action_Boolean action, SteamVR_Input_Sources sources, bool newValue) {
+        if (newValue)
+            Instantiate(obj, hand.transform.position, hand.transform.rotation);
+    }
+
+}
