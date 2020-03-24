b049cbcc743ed5c8ce1a330267c32ce908d7b5c5
linkf206 <62246325+linkf206@users.noreply.github.com>
2020-03-24 21:04:11 +0800

    audio2

---
+++ Assets/FPS/Scripts/GunShot2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunShot2 : MonoBehaviour
+{
+    private AudioSource audiogunshot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audiogunshot = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            audiogunshot.Play();
+        }
+    }
+}
