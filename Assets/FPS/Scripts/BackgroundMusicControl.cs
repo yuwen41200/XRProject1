610d3cb87ec85899f23bc6e55d55191d9c5fd3a5
linkf206 <62246325+linkf206@users.noreply.github.com>
2020-03-24 17:01:50 +0800

    audio

---
+++ Assets/FPS/Scripts/BackgroundMusicControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusicControl : MonoBehaviour
+{
+    private AudioSource background;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        background = GetComponent<AudioSource>();
+        background.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            background.Pause();
+        }
+    }
+}
--- Assets/FPS/Scripts/UI/LoadSceneButton.cs
+++ Assets/FPS/Scripts/UI/LoadSceneButton.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 public class LoadSceneButton : MonoBehaviour
 {
     public string sceneName = "";
-
+    
     private void Update()
     {
         if(EventSystem.current.currentSelectedGameObject == gameObject 
